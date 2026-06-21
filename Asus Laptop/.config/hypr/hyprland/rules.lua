e7a74da3090792007d3af947fe341321a9ef17b4
VoidedKN0X <61878179+VoidedKN0X@users.noreply.github.com>
2026-06-21 22:25:26 +0200

    removed useless code

--- Asus Laptop/.config/hypr/hyprland/rules.lua
+++ Asus Laptop/.config/hypr/hyprland/rules.lua
@@ -31,8 +31,6 @@ hl.window_rule({ match = { xwayland = true, title = "win[0-9]+" }, rounding = 10
 
 hl.layer_rule({ match = { namespace = "swaync-control-center" }, blur = true })
 hl.layer_rule({ match = { namespace = "swaync-notification-window" }, blur = true })
-hl.layer_rule({ match = { namespace = "swaync-control-center" }, ignore_alpha = 0 })
-hl.layer_rule({ match = { namespace = "swaync-notification-window" }, ignore_alpha = 0 })
 hl.layer_rule({ match = { namespace = "swaync-control-center" }, ignore_alpha = 0.5 })
 hl.layer_rule({ match = { namespace = "swaync-notification-window" }, ignore_alpha = 0.5 })
 hl.layer_rule({ match = { namespace = "swaync-control-center" }, animation = "slide left" })
